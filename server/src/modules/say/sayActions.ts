b022ca060a0af1e75174618b1ee4ed7e12743368
alesbesdria <melissa@meirsman.fr>
2025-03-17 12:15:30 +0100

    category monorepo quest 5

--- server/src/modules/say/sayActions.ts
+++ server/src/modules/say/sayActions.ts
@@ -1,11 +1,7 @@
-// Declare the action
-
 import type { RequestHandler } from "express";
-
 const sayWelcome: RequestHandler = (req, res) => {
-  res.send("Welcome to Wild Series !");
+  console.info(req.query);
+  res.send(`Welcome to Wild Series, ${req.query.name} !`);
 };
-
-// Export it to import it somewhere else
 
 export default { sayWelcome };
